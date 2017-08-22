181e1a773268974d4e676043c9dfff51d5157014
OFM <ofm@ofm.clear2pay.com>
2017-08-22 15:20:41 +0200

    Few fixes in preliminaty entity model

--- walledb/src/main/java/com/walle/db/model/Address.java
+++ walledb/src/main/java/com/walle/db/model/Address.java
@@ -1,7 +1,101 @@
 package com.walle.db.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 /**
- * Created by ofm on 8/4/17.
+ * Created by mzwolinski on 8/4/17.
  */
+
+@Entity
 public class Address {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String street;
+    private String homeNumber;
+    private String flatNumber;
+    private String city;
+    private String postaCode;
+    private Short type;
+
+    private Long userId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getHomeNumber() {
+        return homeNumber;
+    }
+
+    public void setHomeNumber(String homeNumber) {
+        this.homeNumber = homeNumber;
+    }
+
+    public String getFlatNumber() {
+        return flatNumber;
+    }
+
+    public void setFlatNumber(String flatNumber) {
+        this.flatNumber = flatNumber;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPostaCode() {
+        return postaCode;
+    }
+
+    public void setPostaCode(String postaCode) {
+        this.postaCode = postaCode;
+    }
+
+    public Short getType() {
+        return type;
+    }
+
+    public void setType(Short type) {
+        this.type = type;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    @Override public String toString() {
+        return "Address{" +
+                "id=" + id +
+                ", street='" + street + '\'' +
+                ", homeNumber='" + homeNumber + '\'' +
+                ", flatNumber='" + flatNumber + '\'' +
+                ", city='" + city + '\'' +
+                ", postaCode='" + postaCode + '\'' +
+                ", type=" + type +
+                ", userId=" + userId +
+                '}';
+    }
 }
--- walledb/src/main/java/com/walle/db/model/User.java
+++ walledb/src/main/java/com/walle/db/model/User.java
@@ -1,7 +1,71 @@
 package com.walle.db.model;
 
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 /**
- * Created by ofm on 8/4/17.
+ * Created by mzwolinski on 8/4/17.
  */
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private Date birthDate;
+    private Short type;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Short getType() {
+        return type;
+    }
+
+    public void setType(Short type) {
+        this.type = type;
+    }
+
+    @Override public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", birthDate=" + birthDate +
+                ", type=" + type +
+                '}';
+    }
 }
