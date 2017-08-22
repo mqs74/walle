286508885ab1265d1e6395ffecef74cd8bc3d3f4
OFM <ofm@ofm.clear2pay.com>
2017-08-22 15:23:38 +0200

    Few fixes in preliminaty entity model

---
+++ walledb/src/main/java/com/walle/db/WalledbApplication.java
@@ -0,0 +1,12 @@
+package com.walle.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalledbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalledbApplication.class, args);
+	}
+}
